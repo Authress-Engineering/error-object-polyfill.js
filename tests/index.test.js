5920d3bf26bc50881e2455b6bb63feffa822f3f4
Warren Parad <wparad@gmail.com>
2018-04-20 21:35:53 +0200

    Revert to ApplicationError, not the best.

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,18 +1,45 @@
 let { describe, it } = require('mocha');
 let { expect } = require('chai');
+require('../index');
 
 describe('index.js', () => {
   describe('Syntax', () => {
     it('correctly captures object in error', () => {
-      let thrownObject = {};
-
-      let error = new Error(thrownObject);
-      expect(error.message).to.equal('[object Object]');
-      
-      require('../index.js');
-
-      error = new Error(thrownObject);
+      let thrownObject = { field: 'value' };
+      let error = new ApplicationError(thrownObject);
       expect(error.message).to.eql(thrownObject);
+    });
+
+    it('It is an error', () => {
+      expect(new ApplicationError({})).to.be.instanceof(Error);
+    });
+
+    it('PolyFill toString works', () => {
+      let toString = new ApplicationError({}).toString();
+      expect(toString).to.equal('ApplicationError: {}');
+    });
+
+    it('callsite stack works', () => {
+      let limit = Error.stackTraceLimit;
+      let obj = {};
+      let prep = Error.prepareStackTrace;
+
+      Error.prepareStackTrace = (a, b) => b;
+      Error.stackTraceLimit = Math.max(10, limit);
+    
+      // capture the stack
+      Error.captureStackTrace(obj);
+    
+      // slice this function off the top
+      let stack = obj.stack.slice(1);
+    
+      Error.prepareStackTrace = prep;
+      Error.stackTraceLimit = limit;
+    
+      let callSite = stack[1];
+      callSite.getFileName();
+      callSite.getLineNumber();
+      callSite.getColumnNumber();
     });
   });
 });
